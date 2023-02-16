6e3276bfdd30bffc51a485c08dec09901437f280
wanjirugachunji <wanjirugachunji@gmail.com>
2023-02-16 23:45:29 +0300

    commit tree ba207210d6b0869ff3b785998735859d2023a9ee

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
+
 class Person
   # your code here
+  @@all=[]
+  def initialize(args)
+    args.each_key do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", args[key])
+    end
+    @@all<<self
+  end
+  def  self.all
+    @@all
+  end
 end
